01f0b178a1aa29332346f57f9d5fc1633dd418c9
Augusto Mina <101650015+Augusto98m@users.noreply.github.com>
2023-01-27 21:18:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menuVisible = false;
+const formulario = document.querySelector('#formulario')
+//Función que oculta o muestra el menú
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
